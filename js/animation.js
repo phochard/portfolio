66c9e627cdfbab21f20b2498f7651f4e63ccacd2
pow <pr62300@gmail.com>
2020-06-03 15:27:00 +0200

    Correction du code.

--- js/animation.js
+++ js/animation.js
@@ -1,23 +1,30 @@
 export function animationOnClick() {
 
     this.revealContent = function() {
-        let items = document.querySelectorAll('#button');
-        items.forEach(item => {
-            item.addEventListener('click', event => {
-                console.log(event);
-                const hiddenContent = document.querySelector('#hidden');
-                hiddenContent.classList.toggle('container--invisible');
+        const items = document.querySelectorAll('#button');
+        for (let i = 0; i < items.length; i++) {
+            items[i].addEventListener('click', e => {
+                const hiddenContent = document.querySelectorAll('#hidden');
+                for (let j = 0; j < hiddenContent.length; j++) {
+                    if (i === j) {
+                        hiddenContent[j].classList.toggle('container--invisible');
+                    }
+                }
             });
-        });
+        };
     }
 
     this.rotateArrow = function() {
-        document.querySelectorAll('.container__break').forEach(item => {
-            item.addEventListener('click', event => {
-                console.log(event);
-                const rotateArrow = document.querySelector('.container__break__chevron');
-                rotateArrow.classList.toggle('container__break__chevron--rotate--down');
+        const arrows = document.querySelectorAll('.container__break');
+        for (let i = 0; i < arrows.length; i++) {
+            arrows[i].addEventListener('click', e => {
+                const rotateArrow = document.querySelectorAll('.container__break__chevron');
+                for (let j = 0; j < rotateArrow.length; j++) {
+                    if (i === j) {
+                        rotateArrow[j].classList.toggle('container__break__chevron--rotate--down');
+                    }
+                }
             });
-        });
+        };
     }
 }
